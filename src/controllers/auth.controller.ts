05bcfd05532f99d1c748eced8cea9b87d116a438
Aarati Thapa <aaratichettri46@gmail.com>
2026-07-09 17:48:20 +0545

    complete the import part

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 import { Request, Response } from "express";
 import { CreateUserDto, LoginUserDto } from "../dtos/user.dtos.ts";
-import { UserService } from "../services/user.services.ts"; //user service import
+import { UserService } from "../services/user.services.ts"; 
 
 let userService = new UserService();
 export class AuthController {
