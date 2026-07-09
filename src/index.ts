da31e84de2316b4a493675455773ce6aca1ec449
Aarati Thapa <aaratichettri46@gmail.com>
2026-07-09 17:41:05 +0545

    check the server part

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { PORT } from "./configs/index.ts";
 import http from "http";
 import { connectDB } from "./database/mangodb.ts";
 
-//server part
 async function startServer(){
     await connectDB();
     const server = http.createServer(app);
