bbd3e3715b0a73b0e52aeb7dea8fc5fa6fca11d9
Aarati Thapa <aaratichettri46@gmail.com>
2026-07-09 22:59:12 +0545

    add posr

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -7,6 +7,6 @@ const router = Router();
 router.post("/register", authController.createUser)
 router.post("/login", authController.loginUser)
 router.put("/profile/:userId", authController.updateUserProfile) // Add this line to handle the update user profile route
-router.post("/profile/upload-photo", authController.uploadProfilePhoto) // Add this line to handle the upload profile photo route
+router.post("/profile/upload-photo", authController.uploadProfilePhoto) 
 
 export default router;
